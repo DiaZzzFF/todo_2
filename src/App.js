f30e2e9b39789a6085e2f28796fc7b160b59756a
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-10-11 18:26:47 +0300

    Добавляет логику Firebase

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import About from "./pages/About"
 import Navbar from "./components/Navbar"
 import Alert from "./components/Alert"
 import AlertState from "./context/alert/AlertState"
+import FirebaseState from "./context/firebase/FirebaseState"
 
 
 const App = () => {
   return (
-    <AlertState>
-      <BrowserRouter>
-        <Navbar />
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
 
-        <div className="container pt-4">
-          <Alert />
+          <div className="container pt-4">
+            <Alert />
 
-          <Switch>
-            <Route path={`/`} exact component={Home} />
-            <Route path={`/about`} component={About} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </AlertState>
+            <Switch>
+              <Route path={`/`} exact component={Home} />
+              <Route path={`/about`} component={About} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
   )
 }
 
---
+++ src/context/firebase/firebaseContext.js
@@ -0,0 +1,7 @@
+import {createContext} from "react"
+
+
+const firebaseContext = createContext()
+
+
+export default firebaseContext
